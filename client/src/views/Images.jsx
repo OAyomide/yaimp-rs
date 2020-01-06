4097fc2edb0897693e806b6b93fcb16bd66397bf
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2020-01-06 02:59:50 +0100

    wasm, client: conditionally apply effect

--- client/src/views/Images.jsx
+++ client/src/views/Images.jsx
@@ -7,165 +7,166 @@ const wasmModule = import('../build')
 // this function coverts our array buffer of manipulated image to base64 to be used in the image component
 // thank you very much, stackoverflow.. saved me hours 🤗
 function UInt8ArrayToBase64(buffer) {
-    let bin = ''
-    for (let i = 0; i < buffer.length; i++) {
-        bin += String.fromCharCode(buffer[i])
-    }
-    return window.btoa(bin)
+  let bin = ''
+  for (let i = 0; i < buffer.length; i++) {
+    bin += String.fromCharCode(buffer[i])
+  }
+  return window.btoa(bin)
 }
 
 
 class UploadImageComponent extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            manipulatedImage: '',
-            mod: null,
-            inst: null,
-            memoryByte: null,
-            bytes: null,
-            isProcessing: false,
-            effect: 'monochrome', // using monochrome as the default effect,
-            disabledEffect: true, // 💡 an idea could be to make this reflexive of isProcessing i.e make it depend on the state of isProcessing instead of manually specifying the current state everywhere... dunno, just an idea. current implementationw works just great
-            imageMeta: null
+  constructor(props) {
+    super(props)
+    this.state = {
+      manipulatedImage: '',
+      mod: null,
+      inst: null,
+      memoryByte: null,
+      bytes: null,
+      isProcessing: false,
+      effect: 'monochrome', // using monochrome as the default effect,
+      disabledEffect: true, // 💡 an idea could be to make this reflexive of isProcessing i.e make it depend on the state of isProcessing instead of manually specifying the current state everywhere... dunno, just an idea. current implementationw works just great
+      imageMeta: null
+    }
+    this.handleChangeStatus = this.handleChangeStatus.bind(this)
+  }
+
+  async componentDidMount() {
+  }
+
+  // handleChangeStatus method handles the conversion of our image to UInt8Array which we pass to the WebAssembly exported function that applies the effect we want
+  async handleChangeStatus({ meta, file }, status) {
+
+    this.setState({
+      isProcessing: true
+    })
+    if (status === 'done') {
+      this.setState({ isProcessing: false, disabledEffect: false, imageMeta: meta })
+    } else if (status === 'removed') {
+      this.setState({ isProcessing: false })
+    }
+  }
+
+  async handleManipulationTrigger(meta) {
+    let fileBlobURL = meta.previewUrl
+    let fetchBlobURL = await fetch(fileBlobURL)
+    let fileBlob = await fetchBlobURL.blob()
+    let fileReader = new FileReader()
+    fileReader.readAsArrayBuffer(fileBlob)
+    fileReader.onload = event => {
+      let memBuf = new Uint8Array(event.target.result)
+      console.log(`IMAGE MEM BUFF BEFORE PASSING TO WASM IS`, memBuf)
+      wasmModule.then(r => {
+        let rs = r.handle_effect(memBuf, this.state.effect)
+        console.log(`MONOCHROME EFFECT RETURNED...`, rs)
+        if (rs) {
+          let img64 = UInt8ArrayToBase64(rs)
+          this.setState({ manipulatedImage: img64 })
         }
-        this.handleChangeStatus = this.handleChangeStatus.bind(this)
+      }).catch(err => console.log(`ERROR RUNNING IMAGE STUFF IN REACT`, err))
+      this.setState({ isProcessing: false, disabledEffect: false })
     }
+  }
 
-    async componentDidMount() {
+  // changes the message text of the Dropzone upload component
+  handleUploadTextChange(files, extras) {
+    if (extras.reject) {
+      return `Images only`
     }
+    return 'Upload an image'
+  }
 
-    // handleChangeStatus method handles the conversion of our image to UInt8Array which we pass to the WebAssembly exported function that applies the effect we want
-    async handleChangeStatus({ meta, file }, status) {
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      message: e.target.value
+    })
+  }
 
-        this.setState({
-            isProcessing: true
-        })
-        if (status === 'done') {
-            this.setState({ isProcessing: false, disabledEffect: false, imageMeta: meta })
-        } else if (status === 'removed') {
-            this.setState({ isProcessing: false })
-        }
-    }
+  handleEffectValueChange = e => {
+    let value = e.target.value
+    let { imageMeta } = this.state
+    this.setState({ effect: value, isProcessing: true, disabledEffect: true }, async () => {
+      this.handleManipulationTrigger(imageMeta)
+    })
+  }
 
-    async handleManipulationTrigger(meta) {
-        let fileBlobURL = meta.previewUrl
-        let fetchBlobURL = await fetch(fileBlobURL)
-        let fileBlob = await fetchBlobURL.blob()
-        let fileReader = new FileReader()
-        fileReader.readAsArrayBuffer(fileBlob)
-        fileReader.onload = event => {
-            let memBuf = new Uint8Array(event.target.result)
-            console.log(`IMAGE MEM BUFF BEFORE PASSING TO WASM IS`, memBuf)
-            wasmModule.then(r => {
-                let rs = r.monochrome(memBuf)
-                console.log(`MONOCHROME EFFECT RETURNED...`, rs)
+  renderEffectsDropdown() {
+    return (
+      <div>
+        <FormGroup>
+          <Label for="select">Effect</Label>
+          <Input type="select" name="select" id="effectselect" required onChange={e => this.handleEffectValueChange(e)} defaultValue="" disabled={this.state.disabledEffect}>
+            <option value="">Select</option>
+            <option value="monochrome">Monochrome</option>
+            <option value="half-monochrome">Half Monochrome</option>
+            <option value="sepia">Sepia</option>
+          </Input>
+        </FormGroup>
+      </div>
+    )
+  }
 
-                let img64 = UInt8ArrayToBase64(rs)
-                this.setState({ manipulatedImage: img64 })
-            }).catch(err => console.log(`ERROR RUNNING IMAGE STUFF IN REACT`, err))
-            this.setState({ isProcessing: false, disabledEffect: false })
-        }
-    }
-
-    // changes the message text of the Dropzone upload component
-    handleUploadTextChange(files, extras) {
-        if (extras.reject) {
-            return `Images only`
-        }
-        return 'Upload an image'
-    }
-
-    handleChange = (e) => {
-        e.preventDefault();
-        this.setState({
-            message: e.target.value
-        })
-    }
-
-    handleEffectValueChange = e => {
-        let value = e.target.value
-        let { imageMeta } = this.state
-        this.setState({ effect: value, isProcessing: true, disabledEffect: true }, async () => {
-            this.handleManipulationTrigger(imageMeta)
-        })
-    }
-
-    renderEffectsDropdown() {
-        return (
-            <div>
-                <FormGroup>
-                    <Label for="select">Effect</Label>
-                    <Input type="select" name="select" id="effectselect" required onChange={e => this.handleEffectValueChange(e)} defaultValue="" disabled={this.state.disabledEffect}>
-                        <option value="">Select</option>
-                        <option value="monochrome">Monochrome</option>
-                        <option value="half-monochrome">Half Monochrome</option>
-                        <option value="sepia">Sepia</option>
-                    </Input>
-                </FormGroup>
-            </div>
-        )
-    }
-
-    async handleManipulatedImageDownload(e) {
-        e.preventDefault()
-        const { manipulatedImage } = this.state
-        let blob = await (await fetch(`data:application/octet-stream;base64,${manipulatedImage}`)).blob()
-        const url = window.URL.createObjectURL(blob)
-        const link = document.createElement('a')
-        link.href = url
-        link.setAttribute('download', 'yaimpManipulated.png')
-        document.body.appendChild(link)
-        link.click()
-    }
+  async handleManipulatedImageDownload(e) {
+    e.preventDefault()
+    const { manipulatedImage } = this.state
+    let blob = await (await fetch(`data:application/octet-stream;base64,${manipulatedImage}`)).blob()
+    const url = window.URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.href = url
+    link.setAttribute('download', 'yaimpManipulated.png')
+    document.body.appendChild(link)
+    link.click()
+  }
 
 
-    render() {
-        const { manipulatedImage, isProcessing } = this.state
-        // TODO: add jpeg to list of accepted codecs when fixed in wasm
-        return (
-            <div className="images-container">
-                <Container>
-                    <Row>
-                        <Col md={6} sm={12} xs={12}>
-                            <Card>
-                                <CardHeader>
-                                    <span>Please Upload your image here</span>
-                                </CardHeader>
-                            </Card>
-                            <CardBody>
-                                <Dropzone
-                                    onChangeStatus={this.handleChangeStatus}
-                                    onSubmit={this.handleImageSubmit}
-                                    accept='image/png'
-                                    styles={{ previewImage: { maxHeight: '70px', maxWidth: '70px' }, dropzone: { height: 200 }, inputLabel: (files, extra) => extra.reject ? {} : {}, inputLabelWithFiles: (files, extra) => { } }}
-                                    inputContent={this.handleUploadTextChange}
-                                    maxFiles={1}
-                                />
-                            </CardBody>
-                            {this.renderEffectsDropdown()}
-                        </Col>
+  render() {
+    const { manipulatedImage, isProcessing } = this.state
+    // TODO: add jpeg to list of accepted codecs when fixed in wasm
+    return (
+      <div className="images-container">
+        <Container>
+          <Row>
+            <Col md={6} sm={12} xs={12}>
+              <Card>
+                <CardHeader>
+                  <span>Please Upload your image here</span>
+                </CardHeader>
+              </Card>
+              <CardBody>
+                <Dropzone
+                  onChangeStatus={this.handleChangeStatus}
+                  onSubmit={this.handleImageSubmit}
+                  accept='image/png'
+                  styles={{ previewImage: { maxHeight: '70px', maxWidth: '70px' }, dropzone: { height: 200 }, inputLabel: (files, extra) => extra.reject ? {} : {}, inputLabelWithFiles: (files, extra) => { } }}
+                  inputContent={this.handleUploadTextChange}
+                  maxFiles={1}
+                />
+              </CardBody>
+              {this.renderEffectsDropdown()}
+            </Col>
 
-                        <Col>
-                            <Card>
-                                <CardHeader>
-                                    <span>Output of your effect</span>
-                                </CardHeader>
-                            </Card>
-                            <CardBody>
-                                {isProcessing ? <span>Processing your image...</span> : (!manipulatedImage ? <span>Please Upload an Image. Manipulated image appears here</span> :
-                                    <Media src={`data:image/png;base64,${manipulatedImage}`} alt="" style={{ height: '100%', width: '100%', position: 'relative' }}></Media>)}
-                            </CardBody>
+            <Col>
+              <Card>
+                <CardHeader>
+                  <span>Output of your effect</span>
+                </CardHeader>
+              </Card>
+              <CardBody>
+                {isProcessing ? <span>Processing your image...</span> : (!manipulatedImage ? <span>Please Upload an Image. Manipulated image appears here</span> :
+                  <Media src={`data:image/png;base64,${manipulatedImage}`} alt="" style={{ height: '100%', width: '100%', position: 'relative' }}></Media>)}
+              </CardBody>
 
-                            <CardFooter>
-                                <Button type='submit' color='success' disabled={!manipulatedImage ? true : false} onClick={async e => await this.handleManipulatedImageDownload(e)}>Download</Button>
-                            </CardFooter>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        )
-    }
+              <CardFooter>
+                <Button type='submit' color='success' disabled={!manipulatedImage ? true : false} onClick={async e => await this.handleManipulatedImageDownload(e)}>Download</Button>
+              </CardFooter>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default UploadImageComponent
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,9 @@
 use image::GenericImageView;
 use wasm_bindgen::prelude::*;
 
+#[macro_use]
+pub mod log;
+
 #[wasm_bindgen]
 extern "C" {
   fn alert(s: &str);
@@ -25,22 +28,42 @@ pub fn fib(num: u8) -> u8 {
 #[wasm_bindgen]
 pub fn run_fib(num: u8) {
   let fib_res = fib(num);
-  log(&format!("{}", fib_res))
+  console_log!("{:?}", fib_res)
 }
 
 #[wasm_bindgen]
 pub fn image_stuff(im: &[u8]) {
   let uimg = image::load_from_memory(im).unwrap();
 
-  log(&format!("{:?}", uimg.dimensions()));
+  console_log!("{:?}", uimg.dimensions());
+}
+
+#[wasm_bindgen]
+pub fn handle_effect(image_buffer: &[u8], effect: &str) -> Option<Box<[u8]>> {
+  match effect {
+    "monochrome" => Some(monochrome(image_buffer)),
+    "half-monochrome" => {
+      console_log!("Oops! This isnt supported yet");
+      // log(&format!("Oops! This isnt supported yet"));
+      None
+    }
+    "sepia" => {
+      console_log!("Oops! This isnt supported yet");
+      None
+    }
+    _ => {
+      console_log!("Oops! Unknowm format");
+      None
+    }
+  }
 }
 
 #[wasm_bindgen]
 pub fn monochrome(image_buff: &[u8]) -> Box<[u8]> {
-  log(&format!(
+  console_log!(
     "IMAGE BUFF PASSED TO WASM BEFORE PROCESSING IS: {:?}",
     image_buff
-  ));
+  );
   // TODO: remove rayon jpeg feature to support wasm jpeg decode
   let img = image::load_from_memory(image_buff).unwrap().grayscale();
   let mut wr = Vec::new();
